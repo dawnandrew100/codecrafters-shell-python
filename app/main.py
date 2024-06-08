db4a7effb221648104ab73e1ab491d5d901d950d
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-08 08:51:00 -0400

    command test

--- app/main.py
+++ app/main.py
@@ -6,8 +6,14 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    cmds = input()
+    responseHandler(cmds)
 
+def responseHandler(incoming):
+    cmds = incoming.split(" ")
+
+    print(cmds)
+    print(cmds[0])
 
 if __name__ == "__main__":
     main()
