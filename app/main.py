99fe24512912b63b56923ed4065336a082c5edf2
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-08 14:10:20 -0400

    Maybe?

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def responseHandler(incoming):
             if os.path.isfile(f"{path}/{cmd[0]}"):
                 command = " ".join(cmd)
                 os.system(command)
-                continue
+                output = "executed"
         if output == "":
             output = f"{cmd[0]}: command not found\n"
 
