7324d88bfdf759451ccadbfa3793ab9276d476f6
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-08 14:24:17 -0400

    the dreaded new line

--- app/main.py
+++ app/main.py
@@ -16,19 +16,19 @@ def responseHandler(incoming):
     output = ""
 
     if cmd[0] == "echo":
-        output = " ".join(cmd[1:]) + "\n"
+        output = " ".join(cmd[1:])
     elif cmd[0] == "type":
         commands = ["echo","type","exit"]
         if cmd[1] in commands:
-            output = f"{cmd[1]} is a shell builtin\n"
+            output = f"{cmd[1]} is a shell builtin"
         else:
             PATH = str(os.environ.get("PATH"))
             paths = PATH.split(":")
             for path in paths:
                 if isfile(f"{path}/{cmd[1]}"):
-                    output = f"{cmd[1]} is {path}/{cmd[1]}\n"
+                    output = f"{cmd[1]} is {path}/{cmd[1]}"
             if output == "":
-                output = f"{cmd[1]}: not found\n"
+                output = f"{cmd[1]}: not found"
     elif cmd[0] == "pwd":
         output = os.getcwd()
     elif cmd[0] == "exit" and cmd[1] == "0":
@@ -45,9 +45,9 @@ def responseHandler(incoming):
                 executed = True
 
         if output == "" and executed == False:
-            output = f"{cmd[0]}: command not found\n"
+            output = f"{cmd[0]}: command not found"
 
-    sys.stdout.write(output)
+    sys.stdout.write(output + "\n")
     sys.stdout.flush()
 
 if __name__ == "__main__":
