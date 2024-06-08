5d49ab1562f010f273473c30d1c4615cfef06ae9
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-08 14:25:19 -0400

    I tried

--- app/main.py
+++ app/main.py
@@ -16,21 +16,21 @@ def responseHandler(incoming):
     output = ""
 
     if cmd[0] == "echo":
-        output = " ".join(cmd[1:])
+        output = " ".join(cmd[1:]) + "\n"
     elif cmd[0] == "type":
         commands = ["echo","type","exit"]
         if cmd[1] in commands:
-            output = f"{cmd[1]} is a shell builtin"
+            output = f"{cmd[1]} is a shell builtin\n"
         else:
             PATH = str(os.environ.get("PATH"))
             paths = PATH.split(":")
             for path in paths:
                 if isfile(f"{path}/{cmd[1]}"):
-                    output = f"{cmd[1]} is {path}/{cmd[1]}"
+                    output = f"{cmd[1]} is {path}/{cmd[1]}\n"
             if output == "":
-                output = f"{cmd[1]}: not found"
+                output = f"{cmd[1]}: not found\n"
     elif cmd[0] == "pwd":
-        output = os.getcwd()
+        output = os.getcwd() + "\n"
     elif cmd[0] == "exit" and cmd[1] == "0":
         status = 0
         sys.exit(status)
@@ -45,9 +45,9 @@ def responseHandler(incoming):
                 executed = True
 
         if output == "" and executed == False:
-            output = f"{cmd[0]}: command not found"
+            output = f"{cmd[0]}: command not found\n"
 
-    sys.stdout.write(output + "\n")
+    sys.stdout.write(output)
     sys.stdout.flush()
 
 if __name__ == "__main__":
